e414ae987d47b318b8b1b4c2d19438be6abf10fd
vbouvardge1 <victor.bouvard-coconet@u-psud.fr>
2018-11-14 20:54:49 +0100

    exo45 fini

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
-#include "TP_lib.h" //inclu bibliotheque TP_lib
+#include "TP_lib.h"
 
-void main (void)	// initialise le programme
+int main (void)
 {
-	short resultat, i=0; // defini les variables resultat et i sur 2octet avec i=0
-	while(i<10) // lance une boucle tant que i<10
+	int short total, val;
+	val=LancerDe();
+	while(val>1)
 	{
-		resultat=LancerDe(); // resultat prend la valeur de la fonction LancerDe()
-		i++;// la nouvelle valeur de i sera i+1
+		if(val==2 || val==4 || val==6)
+		total=total+val;
+		else if(val==3)
+		total=total*2;
+		else if(val==5 && total>0)
+		total=total-2;
 	}
+	return 0;
 }
