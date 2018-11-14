6fb5237eec59023b92b3e017d106fd022804818c
vbouvardge1 <44798449+vbouvardge1@users.noreply.github.com>
2018-11-14 21:02:29 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,11 +1,36 @@
-#include "TP_lib.h" //inclu bibliotheque TP_lib
+#include "my_header.h"
+#include <math.h>
+#include <stdlib.h>
 
-void main (void)	// initialise le programme
+int main(void)
 {
-	short resultat, i=0; // defini les variables resultat et i sur 2octet avec i=0
-	while(i<10) // lance une boucle tant que i<10
-	{
-		resultat=LancerDe(); // resultat prend la valeur de la fonction LancerDe()
-		i++;// la nouvelle valeur de i sera i+1
+     int long S2=0,S3=0,S4=0;
+	 int n2=0,n3=0,n4=1;
+	 int a=((rand())%10)+1;
+	 int c=1;
+	  
+	  
+	 while(S2<9876){
+    S2=S2+3*n2;
+    n2=n2+1; 
+	 }
+	 
+
+	while(n3<12) {
+		S3=S3+a;
+		n3=n3+1;
 	}
+
+	   
+	 while(n4<20){
+		if(c%3==0 || c%7==0){
+			S4=S4+c;
+			c=c+1;
+			n4=n4+1;
+	  }
+	  else
+		  c=c+1;
+	 }
+	  
+    return 0;
 }
