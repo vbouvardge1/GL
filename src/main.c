3a8dd02737b099e6729487166168c79dea69fbc4
victor bouvard <victor.bouvard@ge1.iut-cachan.u-psud.fr>
2018-11-20 09:47:14 +0100

    exo56

--- src/main.c
+++ src/main.c
@@ -1,9 +1,25 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#define seuilb -100
+#define seuilh 200
 
-#include "my_header.h"
 
 int main(void)
 {
-    return 0;
+  unsigned char bin[10]={1,2,3,4,5,6,7,8,9,10};
+   unsigned char i;
+   unsigned char sat[10];
+   
+  
+   for (i=0; i<10; i=i+1)
+   {
+	if (i<5)
+		{	bin[i]=1;
+		}
+	else
+	{	bin[i]=0;
+	}
+   }
+   
+   for (i=rand(); 
+   
+	return 0;
 }
