f66b750acf11fd6ba305a064d6cb1c3db93fb0ea
vbouvardge1 <victor.bouvard-coconet@u-psud.fr>
2018-11-27 20:14:27 +0100

    finis

--- src/main.c
+++ src/main.c
@@ -1,19 +1,29 @@
 #include "TP_lib_suite.h"
 
-initialiserTirage()
-{
-unsigned char gagnant[6]={tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()};
-}
 
+
+	
 int main (void)
 {
-	 unsigned char joueur[6]={3,5,20,10,1,6};
-	 unsigned char i;
-	 unsigned char nbr_gagnant=0;
+	
+	int j;
+	unsigned char i;
+	 unsigned char nbr_gagnant, meilleur=0;
+	 unsigned char joueur[6]={2,15,26,11,34,4}; 
 	 
+	for ( j=0; j<100; j++)
+	{
 	 initialiserTirage();
+	 unsigned short gagnant[6]={tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()};
+
+
+
+	 	 nbr_gagnant=0;
+	 
 	 for ( i=0;i<6;i++)
 	 {
+		
+		
 		 if(joueur[0]==gagnant[i])
 			 nbr_gagnant=nbr_gagnant+1;
 		 if(joueur[1]==gagnant[i])
@@ -26,7 +36,16 @@ int main (void)
 			 nbr_gagnant=nbr_gagnant+1;
 		 if(joueur[5]==gagnant[i])
 			 nbr_gagnant=nbr_gagnant+1;
+	
 	 }
+
+	  	 if (nbr_gagnant > meilleur)
+		 meilleur = nbr_gagnant;
 	 
+
+}
+	 
+
 	 return 0 ;
 }
+		 
