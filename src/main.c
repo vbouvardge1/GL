02adde52c4cc337ffcabf521caaad0d7e52d3307
victor bouvard <victor.bouvard@ge1.iut-cachan.u-psud.fr>
2018-11-27 09:47:14 +0100

    vbouvardge1

--- src/main.c
+++ src/main.c
@@ -1,9 +1,32 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include "TP_lib_suite.h"
 
-#include "my_header.h"
-
-int main(void)
+initialiserTirage()
 {
-    return 0;
+unsigned char gagnant[6]={tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()};
+}
+
+int main (void)
+{
+	 unsigned char joueur[6]={3,5,20,10,1,6};
+	 unsigned char i;
+	 unsigned char nbr_gagnant=0;
+	 
+	 initialiserTirage();
+	 for ( i=0;i<6;i++)
+	 {
+		 if(joueur[0]==gagnant[i])
+			 nbr_gagnant=nbr_gagnant+1;
+		 if(joueur[1]==gagnant[i])
+			 nbr_gagnant=nbr_gagnant+1;
+		 if(joueur[2]==gagnant[i])
+			 nbr_gagnant=nbr_gagnant+1;
+		 if(joueur[3]==gagnant[i])
+			 nbr_gagnant=nbr_gagnant+1;
+		 if(joueur[4]==gagnant[i])
+			 nbr_gagnant=nbr_gagnant+1;
+		 if(joueur[5]==gagnant[i])
+			 nbr_gagnant=nbr_gagnant+1;
+	 }
+	 
+	 return 0 ;
 }
