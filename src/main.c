35a1e6d33929b1896fde177cd95dc878eb96cf04
victor bouvard <victor.bouvard@ge1.iut-cachan.u-psud.fr>
2018-12-04 09:44:39 +0100

    finis

--- src/main.c
+++ src/main.c
@@ -1,9 +1,28 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include <userint.h>
+#include <ansi_c.h>
 
-#include "my_header.h"
+int main (void)
+{ 
+	char phrase[100];
+	char prenom [100];
+	char nom [100];
+	char msg[100];
+	long unsigned int age;
+	long unsigned int annee;
 
-int main(void)
-{
-    return 0;
+
+
+scanf("%s %s %lu" ,prenom,nom,&annee);
+
+age=2018-annee;
+
+printf("je m'appelle %s %s et j'ai %lu ans",prenom,nom,age);
+
+sprintf(msg,"je m'appelle %s %s et j'ai %lu ans",prenom,nom,age);
+MessagePopup("Bonjour",msg); 
+
+
+
+
+return 0 ;
 }
